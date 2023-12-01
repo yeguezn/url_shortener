8a402c5f1a52ae427b6e0adf96db16b8353aba32
yeguezn <yegueznelson7@gmail.com>
2023-12-01 10:00:00 -0400

    Fourth commit

--- shortener/static/js/dom_printer.js
+++ shortener/static/js/dom_printer.js
@@ -4,7 +4,7 @@ function domPrinter(output) {
 
     if ("error_message" in output) {
         fullUrlInput.style.border = "2px solid red";
-        fullUrlInput.value = output["message"];
+        fullUrlInput.value = output["error_message"];
     }
     else if ("shortened_url" in output){
         shortenedUrl.value = output['shortened_url'];
